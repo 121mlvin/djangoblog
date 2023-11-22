47ea9fd6eeb2f31396a715a5aef4e3a6322a8ec1
Ilya <mercy.200111@gmail.com>
2023-11-22 15:01:52 +0200

    Implemented html files to urls

--- myblog/usersapp/urls.py
+++ myblog/usersapp/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
+    path('', views.main, name='main page'),
     path('profile/<str:username>/', views.profile_page),
-    path('about/', views.about),
+    path('about/', views.about, name='about'),
     path('set-password/', views.set_password),
     path('set-userdata/', views.set_userdata),
     path('deactivate/', views.deactivate),
-    path('register/', views.register),
-    path('login/', views.login),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('logout/', views.logout),
 ]
